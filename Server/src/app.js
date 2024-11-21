92bd165cfe95fb34dc40b14b8d29ac31772839b4
Gabriel Gonçalves Albanez <105802493+GabrielAlbanez@users.noreply.github.com>
2024-11-21 11:11:45 -0300

    Delete Server directory

--- Server/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const userRoutes = require("./Routes/userRouter");
-
-const app = express();
-
-app.use(express.json());
-app.use("/users", userRoutes);
-
-module.exports = app;
